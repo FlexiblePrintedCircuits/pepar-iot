077289d0b83bd4ffc8decc3d66f777ae267324ba
YugeHayata <18440@toba.kosen-ac.jp>
2019-02-03 16:57:38 +0900

    new commit

--- main.py
+++ main.py
@@ -1,43 +1,61 @@
+import os
+import sys
 from flask import Flask, request, abort
 
 from linebot import (
- LineBotApi, WebhookHandler
+    LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
- InvalidSignatureError
+    InvalidSignatureError
 )
 from linebot.models import (
- MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage,
 )
 
 app = Flask(__name__)
 
-# 自分のLINE botのデータを設定する。
-line_bot_api = LineBotApi('KFwpfQ+ftxQRMW7yANM+tJSr2c2v/pEyI4w8hViLr9Yfe8o6KohebcwjZHB79Kk+2vUTEKJ4bC0ZPLm9/Ipg/akXtiZORldfra3o2korfV9dBatRCr9uR246Dqm6TTyxosRKlMP/SyQxrJ9xC9mxjAdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('3df819265eae58584bb90f44c95c07ce')
+# 環境変数からchannel_secret・channel_access_tokenを取得
+channel_secret = os.environ['3df819265eae58584bb90f44c95c07ce']
+channel_access_token = os.environ['KFwpfQ+ftxQRMW7yANM+tJSr2c2v/pEyI4w8hViLr9Yfe8o6KohebcwjZHB79Kk+2vUTEKJ4bC0ZPLm9/Ipg/akXtiZORldfra3o2korfV9dBatRCr9uR246Dqm6TTyxosRKlMP/SyQxrJ9xC9mxjAdB04t89/1O/w1cDnyilFU=']
+
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+@app.route("/")
+def hello_world():
+    return "hello world!"
 
 @app.route("/callback", methods=['POST'])
 def callback():
- # get X-Line-Signature header value
- signature = request.headers['X-Line-Signature']
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-# get request body as text
- body = request.get_data(as_text=True)
- http://app.logger.info ("Request body: " + body)
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-# handle webhook body
- try:
-  handler.handle(body, signature)
- except InvalidSignatureError:
-  abort(400)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
- return 'OK'
+    return 'OK'
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
- line_bot_api.reply_message(
-  event.reply_token,
-  TextSendMessage(text=event.message.text))
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
 
 if __name__ == "__main__":
- http://app.run (port=5000)
+    app.run()
