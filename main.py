751b8dd9cdd6a71ab3bd7bf4b070fdf4d2d06d3a
YugeHayata <18440@toba.kosen-ac.jp>
2019-02-05 16:44:55 +0900

    new commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, abort
+import os
+import sys
+from flask import Flask, request, abort, send_file
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -7,19 +9,28 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, LocationMessage, MessageImagemapAction, ImagemapArea, ImagemapSendMessage, BaseSize, LocationSendMessage
 )
-import os
 
 app = Flask(__name__)
 
-#LINE Access Token
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["DqUsUzaNE1qSjeqMYKAVKTnO1DFw2nnE/MvvgQp8cI8WZOCuhi8Hemr8LUcpwX5X2vUTEKJ4bC0ZPLm9/Ipg/akXtiZORldfra3o2korfV8jEDQOTc2OWK00eBenWnoDFJNrRT0e0KzOvbv4qdBRoAdB04t89/1O/w1cDnyilFU="]
-#LINE Channel Secret
-YOUR_CHANNEL_SECRET = os.environ["3df819265eae58584bb90f44c95c07ce"]
+# 環境変数から各種KEYを取得
+channel_secret = os.environ['3df819265eae58584bb90f44c95c07ce']
+channel_access_token = os.environ['DqUsUzaNE1qSjeqMYKAVKTnO1DFw2nnE/MvvgQp8cI8WZOCuhi8Hemr8LUcpwX5X2vUTEKJ4bC0ZPLm9/Ipg/akXtiZORldfra3o2korfV8jEDQOTc2OWK00eBenWnoDFJNrRT0e0KzOvbv4qdBRoAdB04t89/1O/w1cDnyilFU=']
 
-line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+@app.route("/")
+def hello_world():
+    return "hello world!"
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -38,15 +49,38 @@ def callback():
 
     return 'OK'
 
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
-
+    if event.type == "message":
+        if (event.message.text == "あ"):
+            line_bot_api.reply_message(
+                event.reply_token,
+                [
+                    TextSendMessage(text='いうえお')
+                ]
+            )
+        if (event.message.text == "か"):
+            line_bot_api.reply_message(
+                event.reply_token,
+                [
+                    TextSendMessage(text="きくけこ")
+                ]
+            )
+        if event.message.text == "さ":
+            line_bot_api.reply_message(
+                event.reply_token,
+                [
+                    LocationSendMessage(
+                    TextSendMessage(text="しすせそ")
+                ]
+            )
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                [
+                    TextSendMessage(text="まだその言葉は教えてもらってないんです")
+                ]
+            )
 
 if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT"))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
