eaa0505530f54a3ccec7633a1fc10f9918392900
YugeHayata <18440@toba.kosen-ac.jp>
2019-02-05 17:31:30 +0900

    new commit

--- main.py
+++ main.py
@@ -72,7 +72,6 @@ def handle_message(event):
             line_bot_api.reply_message(
                 event.reply_token,
                 [
-                    LocationSendMessage(
                     TextSendMessage(text="しすせそ")
                 ]
             )
