01e1ecb5518532a8d533860b799243324326e723
YugeHayata <18440@toba.kosen-ac.jp>
2019-02-10 12:50:34 +0900

    new commit

--- main.py
+++ main.py
@@ -9,19 +9,12 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
-import os
 
 app = Flask(__name__)
 
-LINE_CHANNEL_ACCESS_TOKEN = os.environ["AJ03tYuaMQXQNXojwbdDSah6XbwAYtpISFrGz7T8A6tVgmRL6wMt3Q2W5qdAPWXY2vUTEKJ4bC0ZPLm9/Ipg/akXtiZORldfra3o2korfV+ooBQ61n0LFbpWjsTxl2X36qIN9x8sCk/xoIesLy7fhQdB04t89/1O/w1cDnyilFU="]
-#channel_access_token = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
-#channel_secret = os.environ["LINE_CHANNEL_SECRET"]
-LINE_CHANNEL_SECRET = os.environ["1a78827b03d52b212fbea6a431718d18"]
+line_bot_api = LineBotApi('AJ03tYuaMQXQNXojwbdDSah6XbwAYtpISFrGz7T8A6tVgmRL6wMt3Q2W5qdAPWXY2vUTEKJ4bC0ZPLm9/Ipg/akXtiZORldfra3o2korfV+ooBQ61n0LFbpWjsTxl2X36qIN9x8sCk/xoIesLy7fhQdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('1a78827b03d52b212fbea6a431718d18')
 
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
-#line_bot_api = LineBotApi(channel_access_token)
-#handler = WebhookHandler(channel_secret)
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -43,10 +36,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text="hogehoge"))
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT"))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
