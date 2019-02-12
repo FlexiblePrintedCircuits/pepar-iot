7df6b4d46cdfff6e8d5a6c5ea72be9a409da7b6f
YugeHayata <18440@toba.kosen-ac.jp>
2019-02-12 17:24:30 +0900

    new commit

--- main.py
+++ main.py
@@ -37,15 +37,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print("aa")
-    if event.type == "message":
-        if (event.message.text == "あ"):
-            line_bot_api.reply_message(
-                event.reply_token,
-                [
-                    TextSendMessage(text='あ')
-                ]
-            )
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 if __name__ == "__main__":
     #app.run()
