a10896b7a00f732dcd4a0dc61157134675246b73
YugeHayata <18440@toba.kosen-ac.jp>
2019-03-07 22:51:58 +0900

    new commit

--- main.py
+++ main.py
@@ -54,19 +54,33 @@ def GetMail():
     Strmaintext=Strmaintext[-6:-4]
     #取得した１６進文字列を数値型１０進に変換
     Intmaintext=int(Strmaintext, 16)
-    return Intmaintext
 
-@app.route("/", methods=['POST'])
+    if (Intmaintext >= 5):
+        return 10
+    elif (Intmaintext < 5) and (Intmaintext >= 4):
+        return 20
+    elif (Intmaintext < 4) and (Intmaintext >= 3):
+        return 30
+    elif (Intmaintext < 3) and (Intmaintext >= 2):
+        return 40
+    elif (Intmaintext < 2):
+        return 50
+
+@app.route("/", methods=['GET'])
 def webhook():
-    PeparDate = request.date[0:2]
-    IntPeparDate = int(PeparDate, 16)
+    PushDis = GetMail()
 
-    if (IntPeparDate <= 10):
+    if PushDis == 10:
         user_id = "Ue8baeea0f29de588e397c74e7b3dcf31"
-        PushMes = TextSendMessage(text="トイレットペーパーが残り少なくなっています！早めの補充を！")
-        line_bot_api.push_message(user_id, messages=PushMes)
 
+        messages = TextSendMessage(text="あと１０ｍ以下です！早めの補充を！")
+        line_bot_api.push_message(user_id, messages=messages)
 
+        messagesImage = ImageSendMessage(
+            original_content_url="https://cdn-ak.f.st-hatena.com/images/fotolife/h/hahayata/20190214/20190214182922.jpg",
+            preview_image_url="https://cdn-ak.f.st-hatena.com/images/fotolife/h/hahayata/20190214/20190214183132.jpg"
+        )
+        line_bot_api.push_message(user_id, messages=messagesImage)
 
 @app.route("/callback", methods=['POST'])
 #この辺はコピペやから何をやっとるかよく分からん
@@ -96,42 +110,42 @@ def make_image_message(PeparDis):
 
     #original_content_urlは、送信した画像をタッチしたら開く画像
     #preview_image_urlは、送信したら勝手に見える画像のプレビュー
-    if (PeparDis <= 50) and (PeparDis > 40):
+    if (PeparDis == 50):
         messages = ImageSendMessage(
             original_content_url="https://cdn-ak.f.st-hatena.com/images/fotolife/h/hahayata/20190214/20190214183132.jpg",
             preview_image_url="https://cdn-ak.f.st-hatena.com/images/fotolife/h/hahayata/20190214/20190214183132.jpg"
         )
         return messages
-    elif (PeparDis <= 40) and (PeparDis > 30):
+    elif (PeparDis == 40):
         messages = ImageSendMessage(
             original_content_url="https://cdn-ak.f.st-hatena.com/images/fotolife/h/hahayata/20190214/20190214182919.jpg",
             preview_image_url="https://cdn-ak.f.st-hatena.com/images/fotolife/h/hahayata/20190214/20190214182919.jpg"
         )
         return messages
-    elif (PeparDis <= 30) and (PeparDis > 20):
+    elif (PeparDis == 30):
         messages = ImageSendMessage(
             original_content_url="https://cdn-ak.f.st-hatena.com/images/fotolife/h/hahayata/20190214/20190214182922.jpg",
             preview_image_url="https://cdn-ak.f.st-hatena.com/images/fotolife/h/hahayata/20190214/20190214182922.jpg"
         )
         return messages
-    elif (PeparDis <= 20) and (PeparDis > 10):
+    elif (PeparDis == 20):
         messages = ImageSendMessage(
             original_content_url="https://cdn-ak.f.st-hatena.com/images/fotolife/h/hahayata/20190214/20190214182925.jpg",
             preview_image_url="https://cdn-ak.f.st-hatena.com/images/fotolife/h/hahayata/20190214/20190214182925.jpg"
         )
         return messages
-    elif (PeparDis <= 10) and (PeparDis > 0):
+    elif (PeparDis == 10):
         messages = ImageSendMessage(
             original_content_url="https://cdn-ak.f.st-hatena.com/images/fotolife/h/hahayata/20190214/20190214182927.jpg",
             preview_image_url="https://cdn-ak.f.st-hatena.com/images/fotolife/h/hahayata/20190214/20190214182927.jpg"
         )
         return messages
-    elif (PeparDis <= 10):
-        messages = ImageSendMessage(
-            original_content_url="https://cdn-ak.f.st-hatena.com/images/fotolife/h/hahayata/20190214/20190214182930.jpg",
-            preview_image_url="https://cdn-ak.f.st-hatena.com/images/fotolife/h/hahayata/20190214/20190214182930.jpg"
-        )
-        return messages
+    #elif (PeparDis <= 10):
+    #    messages = ImageSendMessage(
+    #        original_content_url="https://cdn-ak.f.st-hatena.com/images/fotolife/h/hahayata/20190214/20190214182930.jpg",
+    #        preview_image_url="https://cdn-ak.f.st-hatena.com/images/fotolife/h/hahayata/20190214/20190214182930.jpg"
+    #    )
+    #    return messages
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -140,7 +154,7 @@ def handle_message(event):
             SendMes = GetMail()
             SendImage = make_image_message(SendMes)
             SendMes = str(SendMes)
-            SendMessage = ("あと" + SendMes + "cmです！")
+            SendMessage = ("あと" + SendMes + "m以下です！")
             line_bot_api.reply_message(
                 event.reply_token,
                 [
